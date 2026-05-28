e86b0bedb354a51b89669b05b88a2dfd15093fe2
martabalbo <marta99.balbo@gmail.com>
2026-05-28 12:49:25 +0200

    Add more rows to the grid

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const grid = document.querySelector("div");
 
 for(let i=0; i<16; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
+    const column = document.createElement("div");
+    column.className = "column";
+    for(let j=0; j<16; j++) {
+        const square = document.createElement("div");
+        square.className = "square";
+        //square.textContent = "ciao";
+        column.appendChild(square);
+    }
 
-    grid.appendChild(square);
+    grid.appendChild(column);
 };
+
