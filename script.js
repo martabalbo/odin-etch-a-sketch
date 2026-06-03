5a79f03d67e48863fc8370b6bcb18877b03a5ffb
martabalbo <marta99.balbo@gmail.com>
2026-06-03 21:55:17 +0200

    Add button to change pixels dimension

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector("div");
+let grid = document.querySelector("div");
 
 for(let i=0; i<16; i++) {
     const column = document.createElement("div");
@@ -19,5 +19,43 @@ Array.from(squares).forEach((el) => {
     el.addEventListener('mouseenter', (event) => {
         el.style.backgroundColor = "red";
     });
+});
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    let num = prompt("Please enter a number to decide the pixel size");
+    if (num>100) {
+        num = prompt("Please enter a number under 100");
+    };
+    
+    //grid.remove();
+    let grid = document.querySelector("div");
+
+    let newGrid = document.createElement("div");
+    newGrid.className = "grid";
+    document.body.appendChild(newGrid);
+
+    for(let i=0; i<num; i++) {
+        const column = document.createElement("div");
+        column.className = "column";
+        for(let j=0; j<num; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            //square.textContent = "ciao";
+            column.appendChild(square);
+        }
+
+        newGrid.appendChild(column);
+    };
+    grid.replaceWith(newGrid);
+
+    const squares = document.querySelectorAll('.square');
+
+    Array.from(squares).forEach((el) => {
+        el.addEventListener('mouseenter', (event) => {
+            el.style.backgroundColor = "red";
+        });
+    }); 
 });
 
